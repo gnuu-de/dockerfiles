e202ff8bd22dacee312da663507d94ffc4893514
Frank Kloeker <eumel@admin.gnuu.de>
2020-08-09 21:23:01 +0000

    aio adjustments (cron, start, conf)

--- aio/entrypoint.sh
+++ aio/entrypoint.sh
@@ -4,6 +4,17 @@
 curl -o /var/lib/news/active http://www.gnuu.de/config/active
 curl -o /var/lib/news/newsgroups http://www.gnuu.de/config/newsgroups
 chown news:news /var/lib/news/*
+# merge news config
+if [ -d /etc/news-aio ]; then
+  cp /etc/news-aio/* /etc/news/
+fi
+# build postfix db
+if [ /etc/postfix/transport ]; then
+  postmap /etc/postfix/transport
+fi
+if [ /etc/postfix/virtual ]; then
+  postmap /etc/postfix/virtual
+fi
 # start services
 /usr/bin/shellinaboxd -tb
 /usr/sbin/rsyslogd
