e002711d45d62fedc0a16653584ac1b12cdc6762
Frank Kloeker <eumel@admin.gnuu.de>
2020-08-09 15:39:51 +0000

    add newsgroups/active file, fix pinerc

--- aio/entrypoint.sh
+++ aio/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Catch active & newsgroups file
+curl -o /var/lib/news/active http://www.gnuu.de/config/active
+curl -o /var/lib/news/newsgroups http://www.gnuu.de/config/newsgroupd
+chown news:news /var/lib/news/*
+# start services
 /usr/bin/shellinaboxd -tb
 /usr/sbin/rsyslogd
 /usr/lib/news/bin/rc.news
