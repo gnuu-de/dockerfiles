4fbc0e7f52a2415b0dde76d7f85f1dac3d1db204
Frank Kloeker <eumel@admin.gnuu.de>
2020-08-16 11:58:44 +0000

    add postfix chroot files in postfix-out

--- news/entrypoint.sh
+++ news/entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+# prepare postfix chroot files before service start
+if [ -d "/var/spool/postfix/etc" ]; then
+  cp /etc/services /var/spool/postfix/etc/
+  cp /etc/resolv.conf /var/spool/postfix/etc/
+fi
+# service starts
+# cron for news uucp batcher
 /usr/sbin/cron
+# syslog for newslog sidecar
 /usr/sbin/rsyslogd
+# xinetd for uucp service
 xinetd  -pidfile /run/xinetd.pid -f /xinetd.conf
+# postfix-out for uucp mail out
 postfix start
-#/usr/lib/postfix/sbin/master -c /work-dir/etc/postfix-out 2>&1
+# inn news service runs in foreground
 /usr/lib/news/bin/rc.news
