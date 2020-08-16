e4f084a26bc1b43387c183c0dcc730250afee270
Frank Kloeker <eumel@admin.gnuu.de>
2020-08-16 11:00:37 +0000

    try with postfix command

--- news/entrypoint.sh
+++ news/entrypoint.sh
@@ -3,5 +3,6 @@
 /usr/sbin/cron
 /usr/sbin/rsyslogd
 xinetd  -pidfile /run/xinetd.pid -f /xinetd.conf
-/usr/lib/postfix/sbin/master -c /work-dir/etc/postfix-out 2>&1
+postfix start
+#/usr/lib/postfix/sbin/master -c /work-dir/etc/postfix-out 2>&1
 /usr/lib/news/bin/rc.news
